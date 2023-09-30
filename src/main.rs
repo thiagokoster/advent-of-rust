f1fc7d36b4e81a15453a25c6703bf7464eac2029
Thiago Koster <thiagokoster@gmail.com>
2023-09-30 16:18:38 +0200

    day01 - p1

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,7 @@ use year2022::day01::day01;
 
 fn main() {
     println!("Hello, world!");
-    day01::part1();
+    let part1 = day01::part_1("input.txt");
+
+    println!("Part 1: {}", part1);
 }
--- src/year2022/day01/mod.rs
+++ src/year2022/day01/mod.rs
@@ -1,17 +1,24 @@
 pub mod day01 {
     use advent_of_rust::get_lines;
-    pub fn part1() -> u32 {
-        println!("Hello, day 01!");
-
-        if let Ok(lines) = get_lines("src/year2022/day01/example.txt") {
+    pub fn part_1(input : &str) -> u32 {
+        let mut elves: Vec<u32> = Vec::new();
+        elves.push(0);
+        let mut i = 0;
+        let path = "src/year2022/day01/".to_owned() + input;
+        if let Ok(lines) = get_lines(&path) {
             for line in lines {
                 if let Ok(content) = line {
-                    println!("{}", content);
+                    if let Ok(calories) = content.parse::<u32>() {
+                        elves[i] += calories;
+                    } else {
+                    i += 1;
+                    elves.push(0);
                 }
+                } 
             }
         }
-
-        return 0;
+        elves.sort_by(|a,b| b.cmp(a));
+        return elves[0];
     }
 }
 
@@ -20,8 +27,9 @@ mod tests {
     use super::day01;
 
     #[test]
-    fn test_part1() {
-        let result = day01::part1();
+    fn test_part_1() {
+        let result = day01::part_1("example.txt");
+        println!("Hello, day 01!");
         assert_eq!(result, 24000);
     }
 }
