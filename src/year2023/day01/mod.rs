d79db6a22764eea241a0c6da952f94e038ae455b
Thiago Koster <thiagokoster@gmail.com>
2023-12-01 23:50:03 +0100

    2023/1 - pt2

--- src/year2023/day01/mod.rs
+++ src/year2023/day01/mod.rs
@@ -1,5 +1,3 @@
-use std::str::Chars;
-
 use crate::day::Solution;
 use advent_of_rust::read_file;
 
@@ -26,13 +24,66 @@ impl Solution for Day01 {
     }
 
     fn part_2(&self, input: &str) -> String {
-        "".to_string()
+        let valid_digits = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3",
+            "4", "5", "6", "7", "8", "9",
+        ];
+        let path = BASEPATH.to_owned() + input;
+        let lines = read_file(&path);
+        let mut result = 0;
+
+        for line in lines {
+            let mut number = String::new();
+            number.push(first_digit(&line, &valid_digits));
+            number.push(last_digit(&line, &valid_digits));
+            result += number.parse::<u32>().unwrap();
+        }
+        result.to_string()
     }
 }
 
-fn first_int(chars: Chars) {
-    for c in chars {
-        if c.is_numeric() {}
+fn first_digit(input: &str, valid_digits: &[&str]) -> char {
+    let mut first = (usize::MAX, "");
+    for digit in valid_digits {
+        if !input.contains(digit) {
+            continue;
+        }
+
+        let m = input.match_indices(digit).next().unwrap();
+        if m.0 < first.0 {
+            first = m;
+        }
+    }
+    return convert(first.1);
+}
+
+fn last_digit(input: &str, valid_digits: &[&str]) -> char {
+    let mut last = (0, "");
+    for digit in valid_digits {
+        if !input.contains(digit) {
+            continue;
+        }
+
+        let m = input.match_indices(digit).last().unwrap();
+        if m.0 >= last.0 {
+            last = m;
+        }
+    }
+    return convert(last.1);
+}
+
+fn convert(digit: &str) -> char {
+    match digit {
+        "one" => '1',
+        "two" => '2',
+        "three" => '3',
+        "four" => '4',
+        "five" => '5',
+        "six" => '6',
+        "seven" => '7',
+        "eight" => '8',
+        "nine" => '9',
+        _ => digit.chars().next().unwrap(),
     }
 }
 
@@ -44,5 +95,11 @@ mod tests {
     fn test_part_1() {
         let day = Day01 {};
         assert_eq!(day.part_1("example.txt"), "142");
+    }
+
+    #[test]
+    fn test_part_2() {
+        let day = Day01 {};
+        assert_eq!(day.part_2("example2.txt"), "281");
     }
 }
