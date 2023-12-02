0ae5b289b41f20b7f50084b9ce4760351c8bbdf4
Thiago Koster <thiagokoster@gmail.com>
2023-12-02 16:05:32 +0100

    2023/2 - pt2

--- src/year2023/day02/mod.rs
+++ src/year2023/day02/mod.rs
@@ -12,6 +12,9 @@ struct Bag {
 }
 
 impl Bag {
+    pub fn power(&self) -> u32 {
+        self.red * self.blue * self.green
+    }
     pub fn impossible_game(&self, game: &str) -> bool {
         let red = Bag::check_stone(game, "red", self.red);
         let blue = Bag::check_stone(game, "blue", self.blue);
@@ -36,6 +39,30 @@ impl Bag {
     }
 }
 
+fn minimum_bag(game: &str) -> Bag {
+    Bag {
+        red: minimum_stone(game, "red"),
+        blue: minimum_stone(game, "blue"),
+        green: minimum_stone(game, "green"),
+    }
+}
+
+fn minimum_stone(game: &str, color: &str) -> u32 {
+    let pattern = format!(r"(\d*) {}", color);
+    let re = Regex::new(&pattern).unwrap();
+    let mut min_amount = 0;
+
+    for c in re.captures_iter(game) {
+        let amount = c.get(1).unwrap().as_str().parse::<u32>().unwrap();
+
+        if amount > min_amount {
+            min_amount = amount;
+        }
+    }
+
+    min_amount
+}
+
 impl Solution for Day02 {
     fn part_1(&self, input: &str) -> String {
         let bag = Bag {
@@ -57,7 +84,15 @@ impl Solution for Day02 {
     }
 
     fn part_2(&self, input: &str) -> String {
-        "".to_string()
+        let path = BASEPATH.to_owned() + input;
+        let lines = read_file(&path);
+        let mut result = 0;
+        for game in lines.iter() {
+            let minimum_bag = minimum_bag(game);
+            result += minimum_bag.power();
+        }
+
+        result.to_string()
     }
 }
 
@@ -68,5 +103,10 @@ mod tests {
     fn test_part_1() {
         let day02 = Day02 {};
         assert_eq!(day02.part_1("example.txt"), "8");
+    }
+
+    fn test_part_2() {
+        let day02 = Day02 {};
+        assert_eq!(day02.part_2("example.txt"), "2286");
     }
 }
