150a2514cf88b2090507dbf6ebe2f824b77a5ed3
Thiago Koster <thiagokoster@gmail.com>
2023-12-05 18:45:53 +0100

    implementing map

--- src/year2023/day05/mod.rs
+++ src/year2023/day05/mod.rs
@@ -10,13 +10,73 @@ impl Solution for Day05 {
     fn part_1(&self, input: &str) -> String {
         let path = BASEPATH.to_owned() + input;
         let lines = read_file(&path);
-        for line in lines {
-            println!("{}", line);
-        }
+        parse_input(&lines);
         "".to_string()
     }
     fn part_2(&self, input: &str) -> String {
         "".to_string()
+    }
+}
+
+struct Seed {
+    id: String,
+    soil: String,
+}
+
+#[derive(Debug)]
+struct Map {
+    destination_start: u32,
+    source_start: u32,
+    length: u32,
+}
+
+impl Map {
+    pub fn from_str(line: &str) -> Self {
+        let ids: Vec<u32> = line
+            .splitn(3, " ")
+            .map(|i| i.parse::<u32>().unwrap())
+            .collect();
+        Map {
+            destination_start: ids[0],
+            source_start: ids[1],
+            length: ids[2],
+        }
+    }
+
+    pub fn get_destination(&self, source_id: u32) -> Option<u32> {
+        if source_id >= self.source_start && source_id <= self.source_start + self.length {
+            let diff = source_id - self.source_start;
+            return Some(self.destination_start + diff);
+        }
+        None
+    }
+}
+
+fn parse_input(lines: &Vec<String>) {
+    let colon_index = lines[0].find(":").expect("line should contain a ':'");
+    let seeds = lines[0][colon_index + 1..].trim();
+    let seeds: Vec<u32> = seeds
+        .split(" ")
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+    for seed in seeds {
+        for line in lines[2..].iter() {
+            if line.contains(":") || line.is_empty() {
+                println!("Empty or description line");
+                continue;
+            }
+            let map = Map::from_str(&line);
+            if let Some(destination) = map.get_destination(seed) {
+                println!(
+                    "Map {:?} contains seed {}. Destination: {}",
+                    map, seed, destination
+                );
+            }
+            println!("{}", line);
+        }
+
+        break;
+        !todo!("debugging just the first seed for now");
     }
 }
 
