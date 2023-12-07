a1e27f506986e96d0817db9230fcceed5b4a46cf
Thiago Koster <thiagokoster@gmail.com>
2023-12-07 17:25:08 +0100

    find location

--- src/year2023/day05/mod.rs
+++ src/year2023/day05/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use crate::day::Solution;
 use advent_of_rust::read_file;
 
@@ -21,6 +19,22 @@ impl Solution for Day05 {
 struct Seed {
     id: String,
     soil: String,
+}
+
+struct Maps {
+    maps: Vec<Map>,
+}
+
+impl Maps {
+    pub fn find_destination(&self, source: u32) -> u32 {
+        for map in &self.maps {
+            if let Some(destination) = map.get_destination(source) {
+                return destination;
+            }
+        }
+        // if does not find in all maps, the destination has the same id
+        source
+    }
 }
 
 #[derive(Debug)]
@@ -59,25 +73,52 @@ fn parse_input(lines: &Vec<String>) {
         .split(" ")
         .map(|n| n.parse::<u32>().unwrap())
         .collect();
+
+    let seed_to_soil = get_tag("seed-to-soil", lines);
+    let soil_to_fertilizer = get_tag("soil-to-fertilizer", lines);
+    let fertilizer_to_water = get_tag("fertilizer-to-water", lines);
+    let water_to_light = get_tag("water-to-light", lines);
+    let light_to_temperature = get_tag("light-to-temperature", lines);
+    let temperature_to_humidity = get_tag("temperature-to-humidity", lines);
+    let humidity_to_location = get_tag("humidity-to-location", lines);
+
+    let mut min_location = u32::MAX;
     for seed in seeds {
-        for line in lines[2..].iter() {
-            if line.contains(":") || line.is_empty() {
-                println!("Empty or description line");
-                continue;
-            }
-            let map = Map::from_str(&line);
-            if let Some(destination) = map.get_destination(seed) {
-                println!(
-                    "Map {:?} contains seed {}. Destination: {}",
-                    map, seed, destination
-                );
-            }
-            println!("{}", line);
+        let soil = seed_to_soil.find_destination(seed);
+        let fertilizer = soil_to_fertilizer.find_destination(soil);
+        let water = fertilizer_to_water.find_destination(fertilizer);
+        let light = water_to_light.find_destination(water);
+        let temperature = light_to_temperature.find_destination(light);
+        let humidity = temperature_to_humidity.find_destination(temperature);
+        let location = humidity_to_location.find_destination(humidity);
+
+        min_location = min_location.min(location);
+    }
+
+    println!("{}", min_location.to_string());
+}
+
+fn get_tag(map: &str, input: &Vec<String>) -> Maps {
+    let map = map.to_owned() + " map:";
+    let mut maps: Vec<Map> = Vec::new();
+    let mut tag_idx = 0;
+    if let Some((index, _)) = input
+        .into_iter()
+        .enumerate()
+        .find(|&(_, s)| s == map.as_str())
+    {
+        tag_idx = index + 1;
+    }
+
+    for i in tag_idx..input.len() {
+        if input[i].is_empty() {
+            break;
         }
 
-        break;
-        !todo!("debugging just the first seed for now");
+        maps.push(Map::from_str(&input[i]));
     }
+
+    Maps { maps }
 }
 
 #[cfg(test)]
