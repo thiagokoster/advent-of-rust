4da808cd9e832dd563a35844774cb8e40b631153
Thiago Koster <thiagokoster@gmail.com>
2023-12-07 18:41:05 +0100

    2023/5 - p1

--- src/year2023/day05/mod.rs
+++ src/year2023/day05/mod.rs
@@ -8,56 +8,76 @@ impl Solution for Day05 {
     fn part_1(&self, input: &str) -> String {
         let path = BASEPATH.to_owned() + input;
         let lines = read_file(&path);
-        parse_input(&lines);
-        "".to_string()
+        let (seeds, map) = parse_input(&lines);
+
+        let mut min_location = u64::MAX;
+        for seed in seeds {
+            let location = map.find_seed_location(seed);
+            min_location = min_location.min(location);
+        }
+
+        min_location.to_string()
     }
     fn part_2(&self, input: &str) -> String {
         "".to_string()
     }
 }
 
-struct Seed {
-    id: String,
-    soil: String,
+struct Map {
+    seed_to_soil: Vec<MapEntry>,
+    soil_to_fertilizer: Vec<MapEntry>,
+    fertilizer_to_water: Vec<MapEntry>,
+    water_to_light: Vec<MapEntry>,
+    light_to_temperature: Vec<MapEntry>,
+    temperature_to_humidity: Vec<MapEntry>,
+    humidity_to_location: Vec<MapEntry>,
 }
 
-struct Maps {
-    maps: Vec<Map>,
-}
+impl Map {
+    fn find_seed_location(&self, seed: u64) -> u64 {
+        let soil = Map::get_destination_internal(seed, &self.seed_to_soil);
+        let fertilizer = Map::get_destination_internal(soil, &self.soil_to_fertilizer);
+        let water = Map::get_destination_internal(fertilizer, &self.fertilizer_to_water);
+        let light = Map::get_destination_internal(water, &self.water_to_light);
+        let temperature = Map::get_destination_internal(light, &self.light_to_temperature);
+        let humidity = Map::get_destination_internal(temperature, &self.temperature_to_humidity);
+        let location = Map::get_destination_internal(humidity, &self.humidity_to_location);
 
-impl Maps {
-    pub fn find_destination(&self, source: u32) -> u32 {
-        for map in &self.maps {
-            if let Some(destination) = map.get_destination(source) {
+        location
+    }
+
+    fn get_destination_internal(id: u64, maps: &Vec<MapEntry>) -> u64 {
+        for map in maps {
+            if let Some(destination) = map.get_destination(id) {
                 return destination;
             }
         }
         // if does not find in all maps, the destination has the same id
-        source
+        id
     }
 }
 
 #[derive(Debug)]
-struct Map {
-    destination_start: u32,
-    source_start: u32,
-    length: u32,
+struct MapEntry {
+    destination_start: u64,
+    source_start: u64,
+    length: u64,
 }
 
-impl Map {
+impl MapEntry {
     pub fn from_str(line: &str) -> Self {
-        let ids: Vec<u32> = line
+        let ids: Vec<u64> = line
             .splitn(3, " ")
-            .map(|i| i.parse::<u32>().unwrap())
+            .map(|i| i.parse::<u64>().unwrap())
             .collect();
-        Map {
+        MapEntry {
             destination_start: ids[0],
             source_start: ids[1],
             length: ids[2],
         }
     }
 
-    pub fn get_destination(&self, source_id: u32) -> Option<u32> {
+    pub fn get_destination(&self, source_id: u64) -> Option<u64> {
         if source_id >= self.source_start && source_id <= self.source_start + self.length {
             let diff = source_id - self.source_start;
             return Some(self.destination_start + diff);
@@ -66,12 +86,12 @@ impl Map {
     }
 }
 
-fn parse_input(lines: &Vec<String>) {
+fn parse_input(lines: &Vec<String>) -> (Vec<u64>, Map) {
     let colon_index = lines[0].find(":").expect("line should contain a ':'");
     let seeds = lines[0][colon_index + 1..].trim();
-    let seeds: Vec<u32> = seeds
+    let seeds: Vec<u64> = seeds
         .split(" ")
-        .map(|n| n.parse::<u32>().unwrap())
+        .map(|n| n.parse::<u64>().unwrap())
         .collect();
 
     let seed_to_soil = get_tag("seed-to-soil", lines);
@@ -82,30 +102,27 @@ fn parse_input(lines: &Vec<String>) {
     let temperature_to_humidity = get_tag("temperature-to-humidity", lines);
     let humidity_to_location = get_tag("humidity-to-location", lines);
 
-    let mut min_location = u32::MAX;
-    for seed in seeds {
-        let soil = seed_to_soil.find_destination(seed);
-        let fertilizer = soil_to_fertilizer.find_destination(soil);
-        let water = fertilizer_to_water.find_destination(fertilizer);
-        let light = water_to_light.find_destination(water);
-        let temperature = light_to_temperature.find_destination(light);
-        let humidity = temperature_to_humidity.find_destination(temperature);
-        let location = humidity_to_location.find_destination(humidity);
+    let map = Map {
+        seed_to_soil,
+        soil_to_fertilizer,
+        fertilizer_to_water,
+        water_to_light,
+        light_to_temperature,
+        temperature_to_humidity,
+        humidity_to_location,
+    };
 
-        min_location = min_location.min(location);
-    }
-
-    println!("{}", min_location.to_string());
+    (seeds, map)
 }
 
-fn get_tag(map: &str, input: &Vec<String>) -> Maps {
-    let map = map.to_owned() + " map:";
-    let mut maps: Vec<Map> = Vec::new();
+fn get_tag(map: &str, input: &Vec<String>) -> Vec<MapEntry> {
+    let tag = map.to_owned() + " map:";
+    let mut entries: Vec<MapEntry> = Vec::new();
     let mut tag_idx = 0;
     if let Some((index, _)) = input
         .into_iter()
         .enumerate()
-        .find(|&(_, s)| s == map.as_str())
+        .find(|&(_, s)| s == tag.as_str())
     {
         tag_idx = index + 1;
     }
@@ -115,10 +132,10 @@ fn get_tag(map: &str, input: &Vec<String>) -> Maps {
             break;
         }
 
-        maps.push(Map::from_str(&input[i]));
+        entries.push(MapEntry::from_str(&input[i]));
     }
 
-    Maps { maps }
+    entries
 }
 
 #[cfg(test)]
